a5d6dc9c08caeb95b9b792a18772b04ef2e9aae1
BigJBigJ18 <rai240388@spengergasse.at>
2025-03-21 19:43:36 +0100

    Beschreibung der 2 Konstruktoren bei Aufgabe1

--- src/Aufgabe1.java
+++ src/Aufgabe1.java
@@ -1,3 +1,26 @@
 public class Aufgabe1 {
-    
+
+    /**
+     * Konstruktor für die Instanziierung von Aufgabe1 mit dem Scanner. Der Input muss manuell über die Konsole erfolgen.
+     * Keine Errornachrichten werden durch diesen Konstruktor ausgegeben.
+     */
+    public Aufgabe1(){
+
+    }
+
+    /**
+     * Konstruktor für die Instanziierung von Aufgabe1 mit einem vorgefertigten Array, um Zeit zu sparen und Routinearbeit zu vereinfachen
+     * @param array
+     * int[] array ist für den Array, mit dem das Objekt instanziiert werden soll. Hierbei muss ein vollständiger Array übergeben werden
+     * Throws "NullPointerException: Falsche Instanziierung" bei array==null
+     * Bei limit==true && array.length != wird Errornachricht ausgegeben und die Werte auf den Default gesetzt.
+     *
+     * @param limit
+     * boolean limit steht für die Beschränkung von array.length auf 10.
+     * Wenn limit == true, dann wird array auf die Länge 10 geprüft.
+     * Wenn limit != true, dann wird array direkt zugewiesen (nach Überprüfung auf null).
+     */
+    public Aufgabe1(int[] array, boolean limit){
+
+    }
 }
